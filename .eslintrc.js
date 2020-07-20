88a125d1e69f619f3fcd07d8930f24295796aa0e
condef5 <frank@able.co>
2020-07-19 22:24:57 -0500

    add prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: 'get-off-my-lawn',
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/prop-types': 0,
+    'react/jsx-sort-props': 0,
+    'react/jsx-no-literals': 0,
+    'react/display-name': 0,
+    'react/no-unescaped-entities': 0,
+    'sort-keys': 0,
+    'import/order': 0,
+    'node/no-unpublished-require': 0,
+    'objects/no-object-properties-one-line': 0,
+    'get-off-my-lawn/prefer-arrow-functions': 0
+  }
+};
